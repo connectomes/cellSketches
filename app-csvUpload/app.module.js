fe28fd0d68015a2fc0e717f98370afd32b83bf73
Ethan <ethan.kerzner@gmail.com>
2015-10-04 11:35:39 -0600

    Add first cut of csvUpload

--- app-csvUpload/app.module.js
+++ app-csvUpload/app.module.js
@@ -1,5 +1,21 @@
 (function() {
     'use strict';
 
-    angular.module('app.csvUpload', ['app.volumeModule']);
+    var app = angular.module('app.csvUpload', ['app.volumeModule', 'ui.router']);
+
+    app.config(function($stateProvider, $urlRouterProvider){
+
+        // For any unmatched url, send to /route1
+        $urlRouterProvider.otherwise("/route1");
+
+        $stateProvider
+            .state('route1', {
+                url: "/route1",
+                templateUrl: "upload/route1.html"
+            })
+            .state('route2', {
+                url: "/route2",
+                templateUrl: "vis/route2.html"
+            })
+    });
 })();
---
+++ app-csvUpload/upload/csvUpload.directive.js
@@ -0,0 +1,63 @@
+(function () {
+    'use strict';
+
+    angular.module('app.csvUpload')
+        .directive('csvUpload', csvUpload);
+
+    function csvUpload() {
+
+        return {
+            link: link,
+            restrict: 'E',
+            templateUrl: 'upload/csvUpload.html',
+            controller: controller
+        };
+
+        function link(scope, element, attrs) {
+            console.log('fuck');
+        }
+
+        function controller($scope) {
+
+            $scope.add = function () {
+                // Get file to be input
+                var file = document.getElementById('file').files[0];
+                var reader = new FileReader();
+                reader.onloadend = loadEnd;
+                reader.readAsArrayBuffer(file);
+
+                function loadEnd(e) {
+                    var binary = "";
+                    var bytes = new Uint8Array(e.target.result);
+                    var length = bytes.byteLength;
+
+                    for (var i = 0; i < length; i++) {
+                        binary += String.fromCharCode(bytes[i]);
+                    }
+
+                    var input = (binary).toString();
+                    var lines = input.split('\n');
+                    var sets = [];
+                    for (i = 0; i < lines.length; ++i) {
+                        var currLine = lines[i].split(',');
+                        var name = currLine[0];
+                        var cells = [];
+                        for (var j = 1; j < currLine.length; ++j) {
+                            cells.push(currLine[j]);
+                        }
+                        sets.push({
+                            name: name,
+                            cells: cells
+                        });
+                    }
+                    $scope.cellIdsSelected(sets);
+
+                }
+
+            }
+        }
+
+    }
+
+})
+();
