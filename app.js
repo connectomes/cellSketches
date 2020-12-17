cd6c91b29af2337e4704ca69041aa22bd489fb1a
Ethan Kerzner <ethan.kerzner@gmail.com>
2020-12-17 16:36:16 -0600

    Switch back to HTTPs volumes.

--- app.js
+++ app.js
@@ -51,15 +51,16 @@
                         }
                     ],
 
+
                     selectedMode: {},
                     exportingSvgs: false,
 
                     availableVolumes: [
-                        "http://websvc1.connectomes.utah.edu/RC1/OData/",
-                        "http://websvc1.connectomes.utah.edu/RC2/OData/",
-                        "http://websvc1.connectomes.utah.edu/RPC1/OData/",
-                        "http://websvc1.connectomes.utah.edu/RPC2/OData/",
-                        "http://webdev.connectomes.utah.edu/RC1Test/OData"
+                        "https://websvc1.connectomes.utah.edu/RC1/OData/",
+                        "https://websvc1.connectomes.utah.edu/RC2/OData/",
+                        "https://websvc1.connectomes.utah.edu/RPC1/OData/",
+                        "https://websvc1.connectomes.utah.edu/RPC2/OData/",
+                        "https://webdev.connectomes.utah.edu/RC1Test/OData"
                     ],
                     selectedVolume: "https://websvc1.connectomes.utah.edu/RC1/OData/"
                 },
